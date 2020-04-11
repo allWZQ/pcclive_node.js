e3a969f01a5984453c21af290c5454e894e58480
allWZQ <1933608667@qq.com>
2020-04-11 16:44:07 +0800

    修改样式

---
+++ www/js/base.js
@@ -0,0 +1,165 @@
+"use strict";
+var _typeof =
+  "function" == typeof Symbol && "symbol" == typeof Symbol.iterator
+    ? function (t) {
+        return typeof t;
+      }
+    : function (t) {
+        return t &&
+          "function" == typeof Symbol &&
+          t.constructor === Symbol &&
+          t !== Symbol.prototype
+          ? "symbol"
+          : typeof t;
+      };
+({
+  init: function () {
+    this._bindEvents();
+  },
+  _bindEvents: function () {
+    this._bindFooterEvents(),
+      this._bindProgressBarEvents(),
+      this._bindInstallEvents(),
+      this._bindSearchContentsEvents(),
+      this.openConsoleOutput();
+  },
+  _bindFooterEvents: function () {
+    $("#lastlogo dd")
+      .on("mouseover", function (t) {
+        switch ($($.getTarget(t)).text()) {
+          case "微信":
+            $("#imgshow").css("right", "126px").show();
+            break;
+          case "QQ群":
+            $("#imgshow").css("right", "63px").show();
+            break;
+          case "新浪":
+            $("#imgshow").css("right", "0px").show();
+            break;
+          case "邮箱":
+            $("#imgshow").css("right", "-63px").show();
+        }
+      })
+      .on("mouseout", function () {
+        $("#imgshow").hide();
+      });
+  },
+  _bindProgressBarEvents: function () {
+    function t(t) {
+      (this.progress = t),
+        (this.prg = 0),
+        (this.width = document.documentElement.clientWidth),
+        (this.timer = 0);
+    }
+    (t.prototype = {
+      start: function () {
+        var t = this;
+        t.onchange(),
+          (t.timer = setInterval(function () {
+            t.prg >= t.random([40, 80])
+              ? clearInterval(t.timer)
+              : (t.prg += t.random(3)),
+              (t.progress.style.width = (t.prg / 100) * t.width + "px");
+          }, 15));
+      },
+      onchange: function () {
+        var t = this;
+        document.onreadystatechange = function () {
+          "complete" === document.readyState &&
+            (clearInterval(t.timer),
+            setInterval(function () {
+              100 <= t.prg
+                ? (clearInterval(t.timer),
+                  (t.prg = 100),
+                  (t.progress.style.display = "none"))
+                : (t.prg += t.random(1)),
+                (t.progress.style.width = (t.prg / 100) * t.width + "px");
+            }, 5));
+        };
+      },
+      random: function (t) {
+        if ("object" !== (void 0 === t ? "undefined" : _typeof(t)))
+          return Math.random() * t;
+        var r = t[1] - t[0];
+        return Math.random() * r * 10 + t[0];
+      },
+    }),
+      new t(document.getElementById("progress")).start();
+  },
+  _bindInstallEvents: function () {
+    $("#btn-submit").on("click", function (t) {
+      t.preventDefault();
+      var r = $("#dbname").val(),
+        o = $("#dbip").val(),
+        e = $("#username").val(),
+        n = $("#pwd").val();
+      return "string" != typeof r || 0 === r.length
+        ? ($("#dbname").get(0).focus(),
+          $("#btn-submit").text("立即安装"),
+          alert("数据库名不能为空"))
+        : "string" != typeof o || 0 === o.length
+        ? ($("#dbip").get(0).focus(),
+          $("#btn-submit").text("立即安装"),
+          alert("数据库IP地址不能为空"))
+        : "string" != typeof e || 0 === e.length
+        ? ($("#username").get(0).focus(),
+          $("#btn-submit").text("立即安装"),
+          alert("数据库用户名不能为空"))
+        : "string" != typeof n || 0 === n.length
+        ? ($("#pwd").get(0).focus(),
+          $("#btn-submit").text("立即安装"),
+          alert("数据库用户密码不能为空"))
+        : ($("#btn-submit").text("正在安装……"),
+          void $.ajax({
+            url: "/install",
+            type: "POST",
+            data: { dbname: r, dbip: o, username: e, password: n },
+            dataType: "json",
+            success: function (t) {
+              if (-1 === t.status) {
+                if (t.msg.code)
+                  return (
+                    $("#btn-submit").text("立即安装"),
+                    alert(
+                      "出错了：连接数据库服务器失败，请检查MySQL数据库服务是否已经启动，或数据库主机配置是否正确！"
+                    )
+                  );
+                if (0 < t.msg.indexOf("database"))
+                  return (
+                    $("#btn-submit").text("立即安装"),
+                    alert("数据库名不存在（未知的数据库名）！")
+                  );
+                $("#btn-submit").text("立即安装"),
+                  alert("出错了:" + t.msg + "\n(数据库用户名或密码不正确)！");
+              } else if (1 === t.status)
+                var r = 4,
+                  o = setInterval(function () {
+                    r--,
+                      $("#btn-submit").text(
+                        "安装成功，正准备跳转首页……" + r + "s"
+                      ),
+                      0 === r &&
+                        (clearInterval(o), (window.location.href = "/"));
+                  }, 1e3);
+            },
+          }));
+    });
+  },
+  _bindSearchContentsEvents: function () {
+    $("#intheater").on("click", function () {
+      $("#intheater").css("color", "red").css("font-weight", "bold"),
+        $("#coming").css("color", "black"),
+        $("#top250").css("color", "black");
+    }),
+      $("#coming").on("click", function () {
+        $("#intheater").css("color", "black"),
+          $("#coming").css("color", "red").css("font-weight", "bold"),
+          $("#top250").css("color", "black");
+      }),
+      $("#top250").on("click", function () {
+        $("#intheater").css("color", "black"),
+          $("#coming").css("color", "black"),
+          $("#top250").css("color", "red").css("font-weight", "bold");
+      });
+  },
+}.init());
---
+++ www/js/index.js
@@ -0,0 +1,250 @@
+"use strict";
+var _typeof =
+  "function" == typeof Symbol && "symbol" == typeof Symbol.iterator
+    ? function (t) {
+        return typeof t;
+      }
+    : function (t) {
+        return t &&
+          "function" == typeof Symbol &&
+          t.constructor === Symbol &&
+          t !== Symbol.prototype
+          ? "symbol"
+          : typeof t;
+      };
+!(function () {
+  var e = document.getElementsByTagName("img"),
+    n = document.getElementById("search"),
+    o = document.getElementById("toTop"),
+    r = utils.win("clientHeight");
+  function i() {
+    for (var t = 0; t < e.length; t++) {
+      utils.offset(e[t]).top <
+        utils.win("scrollTop") + utils.win("clientHeight") && a(e[t]);
+    }
+  }
+  function a(t) {
+    var e = t.getAttribute("realImg");
+    if (e && !t.loaded) {
+      var n = new Image();
+      (n.src = e),
+        (n.onload = function () {
+          (t.src = this.src), (n = null), (t.loaded = !0);
+        });
+    }
+  }
+  i(),
+    (o.onclick = function () {
+      var t = utils.win("scrollTop"),
+        e = (t / 500) * 30,
+        n = setInterval(function () {
+          if ((t = utils.win("scrollTop")) <= e)
+            return utils.win("scrollTop", 0), void clearInterval(n);
+          (t -= e), utils.win("scrollTop", t);
+        }, 30);
+    }),
+    (window.onscroll = function () {
+      var t = utils.win("scrollTop");
+      i(),
+        (function () {
+          var t = utils.win("scrollTop");
+          r < t
+            ? ((n.style.display = "block"),
+              (n.style.position = "fixed"),
+              (n.style.zIndex = "3"))
+            : (n.style.display = "none");
+        })(),
+        (o.style.display = r < t ? "block" : "none");
+    });
+})(),
+  (function () {
+    var t = document.getElementById("banner"),
+      e = t.getElementsByTagName("img"),
+      n = t.getElementsByTagName("ul")[0].getElementsByTagName("li"),
+      o = null,
+      r = -1;
+    function i() {
+      ++r > e.length - 1 && (r = 0), a();
+    }
+    function a() {
+      for (var t = 0; t < e.length; t++)
+        t == r
+          ? (utils.css(e[t], "zIndex", 1),
+            animate(
+              e[t],
+              { opacity: 1 },
+              {
+                duration: 600,
+                callback: function () {
+                  for (var t = utils.siblings(this), e = 0; e < t.length; e++)
+                    utils.css(t[e], { opacity: 0 });
+                },
+              }
+            ))
+          : utils.css(e[t], "zIndex", 0);
+      !(function () {
+        for (var t = 0; t < n.length; t++)
+          n[t].className = t == r ? "show" : "";
+      })();
+    }
+    clearInterval(o),
+      (o = setInterval(i, 2e3)),
+      (t.onmouseover = function () {
+        clearInterval(o);
+      }),
+      (t.onmouseout = function () {
+        o = setInterval(i, 2e3);
+      }),
+      (function () {
+        for (var t = 0; t < n.length; t++)
+          !(function (t) {
+            n[t].onmouseover = function () {
+              (r = t), a();
+            };
+          })(t);
+      })();
+  })(),
+  (function () {
+    function t(t) {
+      (this.progress = t),
+        (this.prg = 0),
+        (this.width = document.documentElement.clientWidth),
+        (this.timer = 0);
+    }
+    (t.prototype = {
+      start: function () {
+        var t = this;
+        t.onchange(),
+          (t.timer = setInterval(function () {
+            t.prg >= t.random([40, 80])
+              ? clearInterval(t.timer)
+              : (t.prg += t.random(3)),
+              (t.progress.style.width = (t.prg / 100) * t.width + "px");
+          }, 15));
+      },
+      onchange: function () {
+        var t = this;
+        document.onreadystatechange = function () {
+          "complete" === document.readyState &&
+            (clearInterval(t.timer),
+            setInterval(function () {
+              100 <= t.prg
+                ? (clearInterval(t.timer),
+                  (t.prg = 100),
+                  (t.progress.style.display = "none"))
+                : (t.prg += t.random(1)),
+                (t.progress.style.width = (t.prg / 100) * t.width + "px");
+            }, 5));
+        };
+      },
+      random: function (t) {
+        if ("object" !== (void 0 === t ? "undefined" : _typeof(t)))
+          return Math.random() * t;
+        var e = t[1] - t[0];
+        return Math.random() * e * 10 + t[0];
+      },
+    }),
+      new t(document.getElementById("progress")).start();
+  })(),
+  {
+    moviePageNow: 1,
+    tvPageNow: 1,
+    tvPageNum: 1,
+    init: function () {
+      this._bindEvents(), this._getTvs(), this.openConsoleOutput();
+    },
+    _bindEvents: function () {
+      this._bindHotMovieEvents(), this._bindMovieEvents(), this._bindTvEvents();
+    },
+    _bindHotMovieEvents: function () {
+      var r = this;
+      document
+        .getElementById("change_hot")
+        .addEventListener("click", function (t) {
+          t.preventDefault(),
+            utils.ajax({
+              url: "/hot",
+              type: "POST",
+              dataType: "json",
+              data: { type: "movie" },
+              success: function (t) {
+                if (t.movies) {
+                  var e = t.movies,
+                    n = [e[0], e[1], e[2]],
+                    o = [e[3], e[4], e[5]];
+                  r.renderHotMovies({ list: n }, { list: o });
+                }
+              },
+            });
+        });
+    },
+    renderHotMovies: function (t, e) {
+      var n =
+          '\n                {{#list}}\n                <li>\n                    <a href="/play/{{url}}"><img src="/www/uploads/movie/{{id}}.jpg" realImg="" alt=""></a>\n                    <p>{{title}}</p>\n                    <span>{{info}}</span>\n                </li>\n                {{/list}}',
+        o = utils.renderHtml(n, t) + "",
+        r = utils.renderHtml(n, e) + "";
+      (o = o.trim()),
+        (document.getElementById("hot_movie_top").innerHTML = o),
+        (document.getElementById("hot_movie_bottom").innerHTML = r);
+    },
+    _bindMovieEvents: function () {
+      var e = this,
+        t = document.getElementById("change_movie"),
+        n = document.getElementById("movie_pageNum").value;
+      (n = parseInt(n)),
+        t.addEventListener("click", function (t) {
+          t.preventDefault(),
+            e.moviePageNow < n ? e.moviePageNow++ : (e.moviePageNow = 1),
+            utils.ajax({
+              url: "/index/" + e.moviePageNow,
+              type: "POST",
+              dataType: "json",
+              data: { type: "movie" },
+              success: function (t) {
+                t.movies && e.renderMovies({ list: t.movies });
+              },
+            });
+        });
+    },
+    _bindTvEvents: function () {
+      var e = this;
+      document
+        .getElementById("change_tv")
+        .addEventListener("click", function (t) {
+          t.preventDefault(),
+            e.tvPageNow < e.tvPageNum ? e.tvPageNow++ : (e.tvPageNow = 1),
+            e._getTvs();
+        }),
+        setInterval(function () {
+          e.tvPageNow < e.tvPageNum ? e.tvPageNow++ : (e.tvPageNow = 1),
+            e._getTvs();
+        }, 5e3);
+    },
+    renderMovies: function (t) {
+      var e = utils.renderHtml(
+        '{{#list}}\n                <li class="li1">\n                        <a href="/play/{{url}}" class="pos">\n                            <img src="/www/uploads/movie/{{id}}.jpg" realImg="" alt="">\n                            <em>{{info}}</em>\n                        </a>\n                        <p>{{title}}</p>\n                </li>\n                {{/list}}',
+        t
+      );
+      document.getElementById("movie_container").innerHTML = e;
+    },
+    _getTvs: function () {
+      var e = this;
+      utils.ajax({
+        url: "/index/" + e.tvPageNow,
+        type: "POST",
+        dataType: "json",
+        data: { type: "tv" },
+        success: function (t) {
+          t.tvPageNum && (e.tvPageNum = t.tvPageNum),
+            t.tvs && e.renderTvs({ list: t.tvs });
+        },
+      });
+    },
+    renderTvs: function (t) {
+      var e = utils.renderHtml(
+        '{{#list}}\n                <li class="li1 tv-show">\n                    <a href="/play/{{url}}" class="pos">\n                        <img src="/www/images/tv-loading.gif?id={{url}}}" realImg="" alt="">\n                        <em>{{info}}</em>\n                    </a>\n                    <i><img src="/www/images/vip.png" realImg="" alt=""></i>\n                    <p>{{title}}</p>\n                </li>\n                {{/list}}',
+        t
+      );
+      document.getElementById("tv_container").innerHTML = e;
+    },
+  }.init();
